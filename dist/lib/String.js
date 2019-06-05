91d1c30e5336cd0ffe324407cd35f78176b5689f
Hasan Karahan <hasan.karahan@blackhan.com>
2019-06-05 14:10:04 +0300

    yo @dizmo/module --upgrade

--- dist/lib/String.js
+++ dist/lib/String.js
@@ -1,5 +1,6 @@
 "use strict";
 /* tslint:disable:interface-name */
+
 /**
  * Attaches to the `String` type a `random` function which returns a random
  * string for the provided length and range.
@@ -11,25 +12,30 @@
  */
 
 String.random = function () {
-    var length = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-    var range = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 36;
+  var length = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var range = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 36;
+  length = Math.floor(length);
 
-    length = Math.floor(length);
-    if (length < 0) {
-        throw new Error("length < 0");
-    }
-    if (length > 8) {
-        throw new Error("length > 8");
-    }
-    range = Math.floor(range);
-    if (range < 2) {
-        throw new Error("range < 2");
-    }
-    if (range > 36) {
-        throw new Error("range > 36");
-    }
-    var pow = Math.pow(range, length);
-    var mul = range * pow;
-    return length > 0 ? Math.floor(mul - pow * Math.random()).toString(range).slice(1) : "";
+  if (length < 0) {
+    throw new Error("length < 0");
+  }
+
+  if (length > 8) {
+    throw new Error("length > 8");
+  }
+
+  range = Math.floor(range);
+
+  if (range < 2) {
+    throw new Error("range < 2");
+  }
+
+  if (range > 36) {
+    throw new Error("range > 36");
+  }
+
+  var pow = Math.pow(range, length);
+  var mul = range * pow;
+  return length > 0 ? Math.floor(mul - pow * Math.random()).toString(range).slice(1) : "";
 };
 //# sourceMappingURL=String.js.map
--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,6 +1,9 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 require("./String");
 /**
  * Returns a random string for the provided length and range.
@@ -10,12 +13,14 @@ require("./String");
  *
  * @returns a random string
  */
+
+
 function random() {
   var length = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
   var range = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 36;
-
   return String.random(length, range);
 }
+
 exports.random = random;
-exports.default = random;
+exports["default"] = random;
 //# sourceMappingURL=index.js.map
