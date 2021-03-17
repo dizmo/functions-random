70847d452efb4d971316ff03e02c1e6c9e8a4ded
Hasan Karahan <hasan.karahan@blackhan.com>
2021-03-17 16:55:40 +0300

    DD-4525: deferred require('randombytes')

--- lib/String.ts
+++ lib/String.ts
@@ -3,21 +3,18 @@
 interface StringConstructor {
     random(bytes?: number, encoding?: string): string;
 }
-(() => {
-    const randomBytes = require('randombytes');
-    /**
-     * Attaches to the `String` type a `random` function which returns a random
-     * string for the provided number of bytes and encoding.
-     *
-     * @param bytes
-     *  number of random bytes with `16` as default
-     * @param encoding
-     *  encoding of random string with `hex` as default. Possible encodings are
-     *  `ascii`, `base64`, `hex`, `latin1`, and `ucs2`.
-     *
-     * @returns a random string
-     */
-    String.random = (bytes = 16, encoding = 'hex'): string => {
-        return randomBytes(bytes).toString(encoding);
-    };
-})();
+/**
+ * Attaches to the `String` type a `random` function which returns a random
+ * string for the provided number of bytes and encoding.
+ *
+ * @param bytes
+ *  number of random bytes with `16` as default
+ * @param encoding
+ *  encoding of random string with `hex` as default. Possible encodings are
+ *  `ascii`, `base64`, `hex`, `latin1`, and `ucs2`.
+ *
+ * @returns a random string
+ */
+String.random = (bytes = 16, encoding = 'hex'): string => {
+    return require('randombytes')(bytes).toString(encoding);
+};
